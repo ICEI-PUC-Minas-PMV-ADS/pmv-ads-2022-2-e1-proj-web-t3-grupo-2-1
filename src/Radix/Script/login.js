f38085a6ea395a9c928bcf7d29e400d4ee9ac173
Pablo Alves <pablo9225@gmail.com>
2022-12-08 22:35:30 -0300

    Ajuste em login

--- src/Radix/Script/login.js
+++ src/Radix/Script/login.js
@@ -1,14 +1,23 @@
-function entrar() {
+function validacao() {
     var usuario = document.getElementById('cnpj_cpf').value;
     var senha = document.getElementById('senha').value;
 
     if (usuario === '12345678912' && senha === '123456' || usuario === '12345678912345' && senha === '123456') {
-        alert('Login efetuado com sucesso!')
-        document.location.href = 'https://marlloncas.github.io/pmv-ads-2022-2-e1-proj-web-t3-grupo-2-1/src/Radix/tela_inicial.html';
+        entrar();
+        
+
     } else {
-        alert ('Usuário não cadastrado')
+        alert('Usuário não cadastrado')
         document.getElementById('cnpj_cpf').value = '';
         document.getElementById('senha').value = '';
     }
+}
+
+function dadosOk() {
+    alert('Login efetuado com sucesso!')
+}
+
+function entrar() {
+    document.location.href = 'https://marlloncas.github.io/pmv-ads-2022-2-e1-proj-web-t3-grupo-2-1/src/Radix/tela_inicial.html';
 }
 
